ecae8d7a3a79aec38d3a85928f48cd963bb928ef
Tahir Dibirov II <dibirovtahir@gmail.com>
2023-03-01 12:38:59 +0400

    cart image size a bit reduced

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,12 +4,15 @@ import './Navbar.css'
 
 import { ReactComponent as Logo } from '../../assets/logo.svg'
 import { ReactComponent as Cart } from '../../assets/cart.svg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export const Navbar = () => {
     return (
         <div className='Navbar'>
             <Logo></Logo>
             <div className="links">
+            <FontAwesomeIcon icon={["fal", "coffee"]} />
+        
                 <NavLink className='link' to="/dashboard/">Products</NavLink>
                 <NavLink className='link' to="/dashboard/history">History</NavLink>
                 <NavLink className='link' to="/dashboard/statistics">Statistics</NavLink>
