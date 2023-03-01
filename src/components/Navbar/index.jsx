16445c7710ea977091d03a80ffc31a2fb33020f6
Tahir Dibirov II <dibirovtahir@gmail.com>
2023-03-01 12:12:28 +0400

    Some changes on Navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import './Navbar.css'
 
+import { ReactComponent as Logo } from '../../assets/logo.svg'
+import { ReactComponent as Cart } from '../../assets/cart.svg'
+
 export const Navbar = () => {
     return (
         <div className='Navbar'>
+            <Logo></Logo>
             <div className="links">
-                <NavLink to="/dashboard">Products</NavLink>
-                <NavLink to="/dashboard/history">History</NavLink>
-                <NavLink to="/dashboard/statistics">Statistics</NavLink>
+                <NavLink className='link' to="/dashboard/">Products</NavLink>
+                <NavLink className='link' to="/dashboard/history">History</NavLink>
+                <NavLink className='link' to="/dashboard/statistics">Statistics</NavLink>
             </div>
+            <Cart className="cart"></Cart>
         </div>
     )
 }
