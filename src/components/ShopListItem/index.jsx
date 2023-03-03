d9dd2059f64dcb314c27aa3c26a55583b8224dff
Tahir Dibirov II <dibirovtahir@gmail.com>
2023-03-03 12:29:12 +0400

    Items been created

---
+++ src/components/ShopListItem/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './ShopListItem.css'
+
+export const ShopListItem = () => {
+  return (
+    <div className='ShopListItem'>item 1</div>
+  )
+}
--- src/pages/History/index.jsx
+++ src/pages/History/index.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import './History.css'
 import useTitle from '../../hooks/useTitle'
+import { ShopListItem } from '../../components/ShopListItem'
 
 export const History = () => {
   useTitle('Dashboard / History', [])
   return (
-    <div className='history-page'>History</div>
+    <div className='history-page'>
+      <h1 className='history-title'>Shopping history</h1>
+      <div className="list-items">
+        <ShopListItem/>
+        <ShopListItem/>
+        <ShopListItem/>
+        <ShopListItem/>
+        <ShopListItem/>
+        <ShopListItem/>
+        <ShopListItem/>
+        <ShopListItem/>
+        <ShopListItem/>
+        <ShopListItem/>
+        <ShopListItem/>
+        <ShopListItem/>
+        <ShopListItem/>
+        <ShopListItem/>
+        <ShopListItem/>
+        <ShopListItem/>
+        <ShopListItem/>
+        <ShopListItem/>
+        <ShopListItem/>
+        <ShopListItem/>
+        <ShopListItem/>
+        <ShopListItem/>
+        <ShopListItem/>
+        <ShopListItem/>
+        <ShopListItem/>
+        <ShopListItem/>
+        <ShopListItem/>
+        <ShopListItem/>
+        <ShopListItem/>
+        <ShopListItem/>
+      </div>
+    </div>
   )
 }
