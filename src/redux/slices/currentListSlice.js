e526cc0ce362d670bf748f71fd9045da2c191762
Tahir Dibirov II <dibirovtahir@gmail.com>
2023-03-10 22:05:58 +0400

    Shop list RTK Query config

---
+++ src/redux/slices/currentListSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState = {
+    product: [{name: '', id: '', quantity: 0, bought: '', }]
+}
+
+const currentlistSlice = createSlice({
+    name: 'currentSlice',
+   
+})
