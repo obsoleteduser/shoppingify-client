5c1b29faae1aa1707f2002b436ba38301c513215
Tahir Dibirov II <dibirovtahir@gmail.com>
2023-03-11 12:49:36 +0400

    Shoplist slice has been finished

--- src/redux/slices/currentListSlice.js
+++ src/redux/slices/currentListSlice.js
@@ -2,10 +2,39 @@ import { createSlice } from "@reduxjs/toolkit";
 
 
 const initialState = {
-    product: [{name: '', id: '', quantity: 0, bought: '', }]
+    listName: '',
+    products: [{name: '', id: '', quantity: 0, bought: '', }],
+    status: ''
 }
 
 const currentlistSlice = createSlice({
     name: 'currentSlice',
+    initialState,
+    reducers:{
+        setCurrentList: (state, action) => {
+            return {...state, ...action.payload }
+        },
+        setProduct: (state, action) => {
+            state.products.push(action.payload)
+        },
+        setProductQuantity: (state, action) => {
+            const { productId } = action.payload;
+            const productIndex = state.products.findIndex(product => product.id === productId);
+            if (productIndex !== -1) {
+              state.products[productIndex].quantity += 1;
+            }
+          },
+
+        setProductStatus: (state, action) => {
+           
+            const { productId, status } = action.payload;
+            const productIndex = state.products.findIndex(product => product.id === productId);
+            if (productIndex !== -1) {
+              state.products[productIndex].bought = status;
+            }
+            
+        }
+          
+    }
    
 })
