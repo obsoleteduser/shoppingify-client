46257dcb529908bb826dc5df10b83044f7be4594
Tahir Dibirov II <dibirovtahir@gmail.com>
2023-03-11 18:09:11 +0400

    Done with styles n stuff

--- src/components/ShopListMaker/index.jsx
+++ src/components/ShopListMaker/index.jsx
@@ -6,42 +6,46 @@ import { useSelector } from 'react-redux'
 
 export const ShopListMaker = () => {
 
-  const { toggleAdder, toggleList } = useToggle()
-  const products = useSelector(state => state.currentListReducer.products)
- 
+    const { toggleAdder, toggleList } = useToggle()
+    const products = useSelector(state => state.currentListReducer.products)
+
 
     return (
         <div className='ItemAdder'>
             <div className="list-container">
                 <div className="add-item-bottle-wrapper">
-                <Bottle className='bottle-logo'></Bottle>
                     <div className="add-item-bottle">
-                    <p>
-                    Didn’t find what you need?
-                    </p>
-                    
 
-                    <button onClick={()=>{
-                      toggleAdder()
-                    
-                    }} className='add-item-btn'>Add Item</button>
+                       <div className="text-n-bottle">
+                       <Bottle className='bottle-logo'></Bottle>
+                        <p>
+                            Didn’t find what you need?
+                        </p>
+                       </div>
+
+
+
+                        <button onClick={() => {
+                            toggleAdder()
+
+                        }} className='add-item-btn'>Add Item</button>
                     </div>
                 </div>
 
                 <div className="must-buy-products">
-                        {
+                    {
                         products && products.map(product => (
-                            <div key={product.id} className="must-by-product">
-                               <span>{product.name}</span>
-                               <span>{products.filter(item => item.id===product.id)[0].quantity}</span>
+                            <div key={product.id} className="must-buy-product">
+                                <span className='product-name'>{product.name}</span>
+                                <span className='product-quantity'>{products.filter(item => item.id === product.id)[0].quantity} pcs</span>
                             </div>
                         ))
-                        }
-                    </div>
+                    }
+                </div>
 
             </div>
 
-          
+
 
             <div className="list-name">
                 <input type="text" placeholder='Enter a name' />
