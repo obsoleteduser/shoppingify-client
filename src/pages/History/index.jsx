43fca8554b36eacca6e3912d266a68698d3b3a5e
tahirdibirovresearch <tahirdibirovresearch@gmail.com>
2023-03-25 12:46:55 +0300

    #

--- src/pages/History/index.jsx
+++ src/pages/History/index.jsx
@@ -3,11 +3,19 @@ import './History.css'
 import useTitle from '../../hooks/useTitle'
 import { ShopListItem } from '../../components/ShopListItem'
 import { useGetListsQuery } from '../../redux/api/shopListApi'
+import { PuffLoader } from 'react-spinners'
 
 export const History = () => {
   useTitle('Dashboard / History', [])
   const { data = [], isLoading, error } = useGetListsQuery()
   console.log(data)
+
+  if(isLoading){
+
+    return <div className='spinner'><PuffLoader color="orange" size={100}/></div>
+ }
+
+
   return (
     <div className='history-page'>
       <h2 className='history-title'>Shopping history</h2>
