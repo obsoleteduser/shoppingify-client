7e3945effb624eae8916e3ec84f18af0e4c58ef2
Tahir Dibirov II <dibirovtahir@gmail.com>
2023-03-26 22:59:01 +0400

    #

--- src/pages/ListDetails/index.jsx
+++ src/pages/ListDetails/index.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import Item from '../../components/Item'
 import './ListDetails.css'
 
 export const ListDetails = () => {
 
  const list = useSelector(state => state.listDetailReducer)
+ const navigate = useNavigate()
   console.log(list?.data?.products)
 
   return (
     <div className='list-details'>
+       <button onClick={()=>{navigate(-1)}} className="back-btn">Back</button>
       <h2 className='list-details-title'>List Details</h2>
       <h3 className="list-details-name">{list?.data?.name}</h3>
       <div style={list?.data?.status === 'completed' ? {color: 'skyblue', border: '1px solid skyblue'}: {color: 'red', border: '1px solid red'}} className="list-details-status">{list?.data?.status}</div>
